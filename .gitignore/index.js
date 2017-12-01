c39ff771e8ea7c3940ab95ccc9deca5d60e4b300
FrenchGaming110 <34167900+FrenchGaming110@users.noreply.github.com>
2017-12-01 18:08:57 +0100

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js");
+
+var bot = new Discord.Client();
+
+bot.on("ready", function(){
+    bot.user.setGame("TestBot, .help");
+    console.log("Le bot a bien été connecté");
+})
+
+bot.login("Mzc2MzM5NTg3Njk5ODM0ODgx.DQMT0g.YIHj8HWu2lzxzCHYE0NiLQW8vF4");
